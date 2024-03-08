5ea1af356b5e42c22e59eba6e55f2b88f5727311
wql2002 <wqlthu2020@gmail.com>
2024-03-08 05:42:39 -0500

    finish exercises [enums, strings]

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,38 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // trim frontend
+    let mut front_end = 0;
+    let mut back_end = input.len();
+    for c in input.chars() {
+        if c == ' ' {
+            front_end += 1;
+        } else {
+            break;
+        }
+    }
+    // trim backend
+    for c in input.chars().rev() {
+        if c == ' ' {
+            back_end -= 1;
+        } else {
+            break;
+        }
+    }
+    input[front_end..back_end].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
