bd5f47522b87b13f80345dfd824a7cf75b079f0b
wql2002 <wqlthu2020@gmail.com>
2024-07-23 01:59:08 +0800

    feat: enhence code's readability

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -40,12 +40,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // team_1.
         // get team 1 entry
         let team_1_entry = scores.entry(team_1_name).or_insert(Team{ goals_scored: 0, goals_conceded: 0 });
-        (*team_1_entry).goals_scored += team_1_score;
-        (*team_1_entry).goals_conceded += team_2_score;
+        team_1_entry.goals_scored += team_1_score;
+        team_1_entry.goals_conceded += team_2_score;
         // get team 2 entry
         let team_2_entry = scores.entry(team_2_name).or_insert(Team{ goals_scored: 0, goals_conceded: 0 });
-        (*team_2_entry).goals_scored += team_2_score;
-        (*team_2_entry).goals_conceded += team_1_score;
+        team_2_entry.goals_scored += team_2_score;
+        team_2_entry.goals_conceded += team_1_score;
     }
     scores
 }
