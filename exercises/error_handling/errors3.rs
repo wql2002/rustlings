6b632851ac279897dd68b19ef6b290672d3010b8
wql2002 <wqlthu2020@gmail.com>
2024-07-24 01:24:00 +0800

    feat: finish exercise {error_handling}

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,11 +7,24 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+// fn main() {
+//     let mut tokens = 100;
+//     let pretend_user_input = "8";
+
+//     let cost = total_cost(pretend_user_input).unwrap();
+
+//     if cost > tokens {
+//         println!("You can't afford that many!");
+//     } else {
+//         tokens -= cost;
+//         println!("You now have {} tokens.", tokens);
+//     }
+// }
+
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -23,6 +36,7 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
